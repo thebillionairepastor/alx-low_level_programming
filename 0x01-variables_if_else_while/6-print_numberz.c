bfc20890312bb6133862f68e70ffa3b390312f11
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-08 15:16:14 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,19 +3,17 @@
  * main - program that prints all single digit numbers of base 10 starting from
  * 
  * Return: always 0
+ 
  */
 int main(void)
-  
 {
 int i;
   
-for (i = 0; i <= 57; i++)
+for (i = 0; i <= 9; ++i)
 {
-putchar (i);
-}
+putchar ('0' + i);
   
 putchar ('\n');
   
 return (0);
-  
 }
