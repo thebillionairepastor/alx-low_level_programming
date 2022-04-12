2a0864ae8be9c99a0fb81256c37857c32514bb6c
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-12 14:44:34 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
- * Return: 1 if 
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
  */
-int  _isalpha(int c)
-	if (((c >= 'a' ) && (c <= 'z')) || ((c >= 'A') && (c <= 'z')))
-		return (1);
-	else 	
-		return (0);
-	
+void print_times_table(int n)
+{
+
+	return (0);
+}
