c03f8767ee53ca7ad8e3b05c4939e034b8c67427
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-12 15:04:16 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,18 @@
- #include "holberton.h"
+#include "main.h"
 
 /**
- * print_times_table - function that prints the n times table, starting with 0.
- * @n: input of table n
- * Return: nothing.
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
-void print_times_table(int n)
+int main(void)
 {
-	int row, col;
-	int mult = 0;
-
-	if (n <= 15 && n >= 0)
-	{
-		for (row = 0; row <= n; row++)
-		{
-			for (col = 0; col <= n; col++)
-			{
-				mult = (row * col);
-				if (mult < 10)
-				{
-					if (col != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(mult + '0');
-				}
-				else if (mult < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((mult / 10) + '0');
-					_putchar((mult % 10) + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar((mult / 100) + '0');
-					_putchar(((mult % 100) / 10) + '0');
-					_putchar((mult % 10) + '0');
-				}
-				if (col != n)
-					_putchar(',');
-			}
-			_putchar('\n');
-		}
-	}
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
