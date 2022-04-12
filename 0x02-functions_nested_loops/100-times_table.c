5ca06ce1049cbe916f00790b16f783e236b64338
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-12 14:32:12 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,40 +5,8 @@
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
-void print_times_table(int n)
-{
-	int num, mult, prod;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
-}
+int  _isalpha(int c)
+	if (((c >= 'a' ) && (c<= 'z')) || ((c >= 'A') && (c <= 'z')))
+		return (1) ;
+	else 	return (0);
+	
