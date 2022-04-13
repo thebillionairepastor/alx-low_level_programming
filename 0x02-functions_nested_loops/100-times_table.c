2b5597c386c44c51fbdf1f00197131e0f4bd6e8a
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-13 08:53:17 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - program that prints all possible different combinations of two digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Return: 0
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-int n1 = 48;
-int n2;
-int com = 44;
-while (n1 <= 57)
-{
-n2 = n1 + 1;
-while (n2 <= 57)
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 56 || n2 != 57)
-{
-putchar(com);
-putchar(32);
-}
-n2 += 1;
-}
-n1 += 1;
-}
-putchar('\n');
-return (0);
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
