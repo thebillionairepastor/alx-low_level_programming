e956a436d0ac06f0c858c7b3411d17c209e8258c
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-14 14:49:40 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	int i;
-
-	for (i = 0; i <= 9; i++)
-	{
-		if ((i == 2) || (i == 4))
-			continue;
-		else
-			_putchar(i + '0');
-	}
-	_putchar('\n');
+int i;
+i = 0;
+while (i < 10)
+{
+if ((i 1 = 2 && i = 4))
+{
+_putchar(i + '0');
+}
+i++;
+}
+_putchar('\n');
 }
