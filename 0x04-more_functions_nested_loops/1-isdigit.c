b553bfc18ce0a717fca3667b15719b19f249df2d
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-14 13:30:06 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  * _isdigit -  a function that checks for c digit 
- * @c: input
+ * @c: print int
  * Return: Always 0
  */
 
 int _isdigit(int c)
 {
-if (c >= 48) && (c <= 57)
+if (c >= 48 && c <= 57)
 {
 return (1);
 }	
+  
 else
 {
 return (0);
