1c80519f895f632013a77d5eec08b0098893f4fb
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-14 14:05:36 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - a function that prints the numbers
+ * void print_numbers - a function that prints the numbers
  * Return: Always 0
  */
 void print_numbers(void);
