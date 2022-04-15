192e41954a7c5062c1b3e36e5ebd07f44f4868c8
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-15 07:17:57 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * print_line - a function that draws a straight line in the terminal
- * @n: input number of times to print '_'
- * Return: a straight line
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
-void print_line(int n)
+void print_most_numbers(void)
 {
-	int co;
+	int ch;
 
-	if (n <= 0)
+	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar('\n');
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
 	}
-	else
-	{
-		for (co = 1; co <= n; co++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
