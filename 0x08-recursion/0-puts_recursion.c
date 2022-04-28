ee43eb8e338a7f30ac437dbda504a35928d52353
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-28 15:46:23 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _puts_recursion - print a string followed by a newline
- * @s: a pointer to the string to print
+ * _puts_recursion -  function that prints a string
  *
- * Return: void
+ * @s: string to receive
+ *
+ * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
