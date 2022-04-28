f1da6c2d7a499aed6b9dd4ecfca72d8e727d72d6
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-28 15:43:32 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - print a string in reverse
- * @s: a pointer to the string to print in reverse
- *
- * Return: void
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: character of a string
+ * Return: Always 0.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
