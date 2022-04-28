500d0393fce05c345e02364f51cd8c9fdff6cd5d
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-28 15:17:25 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
+ * _print_rev_recursion - Prints a string in reverse
  * @s: string
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != '\0')
 	{
-		return 0;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	printf(*s);
 }
