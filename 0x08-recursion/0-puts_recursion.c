99989e0b6ef5098de4581322598dec4186e5ac84
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-04-28 14:05:58 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void _puts_recursion(char *s)
 {
