d15a89cc43a39b0501a23a2eb2c1b9967c6af669
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-05-26 13:08:19 +0100

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,19 +1,27 @@
+/*
+ * File: 8-sum_listint.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of a listint_t list.
- * @head: pointer to the list.
- * Return: sum of all data in the list, 0 if the lost is empty.
- **/
+ * sum_listint - Calculates the sum of all the
+ *               data (n) of a listint_t list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: If the list is empty - 0.
+ *         Otherwise - the sum of all the data.
+ */
 int sum_listint(listint_t *head)
 {
-	listint_t *node = head;
 	int sum = 0;
 
-	while (node)
+	while (head)
 	{
-		sum += node->n;
-		node = node->next;
+		sum += head->n;
+		head = head->next;
 	}
 
 	return (sum);
+}
